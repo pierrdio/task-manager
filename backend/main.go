13b72fedf3a5e1c22ee87765203ab347302892c9
PierreDioJ <maks-jupcev@mail.ru>
2026-09-22 01:33:02 +0300

    get user by id

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type User struct {
@@ -20,13 +22,50 @@ var users = []User{
 }
 
 func usersHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
+	if r.Method == http.MethodGet {
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(users)
 		return
 	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	fmt.Fprintln(w, "method not allowed")
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		var user User
+
+		idStr := strings.TrimPrefix(r.URL.Path, "/users/")
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, "invalid ID")
+			return
+		}
+
+		for _, u := range users {
+			if u.ID == id {
+				user = u
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(user)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, "not found")
+		return
+	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	fmt.Fprintln(w, "method not allowed")
+	return
 }
 
 const PORT = ":8080"
@@ -38,6 +77,7 @@ func main() {
 	})
 
 	http.HandleFunc("/users", usersHandler)
+	http.HandleFunc("/users/", userHandler)
 
 	fmt.Println("Server started on port", PORT)
 
